398d87e3dee20de59c4b695a2639af5c86402c2d
mkonemo <mkonemo@gmail.com>
2021-10-08 16:23:03 +0200

    1 file modified

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const AddUser = () => {
-  return (
-    
+
+  const addUserHandler = () => {
+      
+  }
+
+  return (    
+    <form onSubmit={}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
+      </form>
   );
 }
 
