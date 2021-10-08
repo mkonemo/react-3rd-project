6a85d7b985f66f92a11c82c30d538f4ae2d3fd95
mkonemo <mkonemo@gmail.com>
2021-10-08 16:17:07 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Helo StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      
     </div>
   );
 }
