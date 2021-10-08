231107aabe5f63f9e2f9745338343d6403f4a963
mkonemo <mkonemo@gmail.com>
2021-10-08 17:17:03 +0200

    1 file modified, 1 added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
 const AddUser = () => {
-
   const addUserHandler = (event) => {
-      event.preventDefault();
-  }
+    event.preventDefault();
+  };
 
-  return (    
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
+  return (
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
       </form>
+    </Card>
   );
-}
+};
 
 export default AddUser;
