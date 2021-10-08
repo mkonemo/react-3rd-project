e9dc4086d7654a80f77fb3aa4afd49ce957df2c6
mkonemo <mkonemo@gmail.com>
2021-10-08 16:24:11 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
 
+import AddUser from "./components/Users/AddUser"
+
 export default function App() {
   return (
     <div>
-      
+      <AddUser/>
     </div>
   );
 }
