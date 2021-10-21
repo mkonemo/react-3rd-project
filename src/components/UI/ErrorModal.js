2e87bc6cd1566171adf90f83b809321789d731c2
mkonemo <mkonemo@gmail.com>
2021-10-21 15:52:35 +0200

    2 files added

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Card from './Card';
+import Buton from './Button';
+
+import classes from './ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+  return (
+    <Card>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
+      </div>
+      <footer>
+        <Button>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
+export default ErrorModal;
